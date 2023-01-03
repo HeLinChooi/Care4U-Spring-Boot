7615d2b974f8e9c49ada97bd7ec54a79177b5221
CWH <55231366+CyberWorldHao@users.noreply.github.com>
2023-01-03 22:18:01 +0800

    Weihao (#4)
    * Fixing setup env to print hello world
    
    * Patient Profile - Create & Update

---
+++ src/main/java/com/example/controller/PatientController.java
@@ -0,0 +1,62 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Patient;
+import com.example.service.PatientService;
+
+@RestController
+public class PatientController {
+  @Autowired
+  private PatientService service;
+
+  @PostMapping("/patient")
+  public Patient addPatient(@RequestBody Patient patient) {
+    return service.savePatient(patient);
+  }
+
+  @GetMapping("/patients") // yongming test
+  public List<Patient> findAllPatients() {
+    return service.getPatients();
+  }
+
+  @GetMapping("/patient-by-id/{id}") // yongming test
+  public Patient findPatientById(@PathVariable int id) {
+    return service.getPatientById(id);
+  }
+
+  @GetMapping("/patient/{email}") // yongming test
+  public Patient findPatientByEmail(@PathVariable String email) {
+    return service.getPatientByEmail(email);
+  }
+
+  @GetMapping("/patients/{name}") // yongming test
+  public Patient findPatientByName(@PathVariable String name) {
+    return service.getPatientByName(name);
+  }
+
+  @PutMapping("/patient")
+  public Patient updatePatient(@RequestBody Patient patient) {
+    return service.updatePatient(patient);
+  }
+
+  @DeleteMapping("/delete-patient-by-id/{id}") // yongming test
+  public String deletePatient(@PathVariable int id) {
+    return service.deletePatientById(id);
+  }
+
+  @DeleteMapping("/delete-patient-by-email/{email}") // yongming test
+  public String deletePatient(@PathVariable String email) {
+    return service.deletePatientByEmail(email);
+  }
+
+}
---
+++ src/main/java/com/example/service/PatientService.java
@@ -0,0 +1,55 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.model.Patient;
+import com.example.repository.PatientRepository;
+
+@Service
+public class PatientService {
+  @Autowired
+  private PatientRepository repository;
+
+  public Patient savePatient(Patient patient) {
+    return repository.save(patient);
+  }
+
+  public List<Patient> getPatients() {
+    return repository.findAll();
+  }
+
+  public Patient getPatientById(int id) {
+    return repository.findById(id).orElse(null);
+  }
+
+  public Patient getPatientByEmail(String email) {
+    return repository.findByEmail(email);
+  }
+
+  public Patient getPatientByName(String name) {
+    return repository.findByName(name);
+  }
+
+  public String deletePatientById(int id) {
+    repository.deleteById(id);
+    return "patient profile removed: " + id;
+  }
+
+  public String deletePatientByEmail(String email) {
+    repository.deleteByEmail(email);
+    return "patient profile removed: " + email;
+  }
+
+  public Patient updatePatient(Patient patient) {
+    Patient existingPatient = repository.findById(patient.getId()).orElse(null);
+    existingPatient.setName(patient.getName());
+    existingPatient.setAge(patient.getAge());
+    existingPatient.setGender(patient.isGender());
+    existingPatient.setPhoneNo(patient.getPhoneNo());
+    existingPatient.setEmail(patient.getEmail());
+    return repository.save(existingPatient);
+  }
+}
