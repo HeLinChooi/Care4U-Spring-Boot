2c7a4fc508638a3f9833dd104287d3a55f48b95d
CWH <ahaoahao2000@gmail.com>
2023-01-12 04:19:10 +0800

    Minor Amendment

--- src/main/java/com/example/controller/MedicalRecordController.java
+++ src/main/java/com/example/controller/MedicalRecordController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,30 +31,31 @@ public class MedicalRecordController {
   PatientRepository patientRepository;
 
   @PostMapping("/medical-record")
-  public MedicalRecord addMedicalRecord(@RequestBody MedicalRecordDto medicalRecordDto) {  
+  public MedicalRecord addMedicalRecord(@RequestBody MedicalRecordDto medicalRecordDto) {
     Optional<Patient> optionalPatient = patientRepository.findById(medicalRecordDto.getPatient_id());
-    if(!optionalPatient.isPresent()){
+    if (!optionalPatient.isPresent()) {
       throw new CustomException("Cannot find this patient.");
     }
-    return service.saveMedicalRecord(medicalRecordDto,optionalPatient.get());
+    return service.saveMedicalRecord(medicalRecordDto, optionalPatient.get());
   }
-  
+
   @GetMapping("/medical-records")
   public List<MedicalRecordDto> findAllMedicalRecords() {
     return service.getMedicalRecords();
-  } 
+  }
 
   @GetMapping("/medical-record-by-id/{id}")
-  public MedicalRecord findMedicalRecordById(@PathVariable int id){
+  public MedicalRecord findMedicalRecordById(@PathVariable int id) {
     if (service.getMedicalRecordById(id) == null) {
       throw new CustomException("Cannot find this patient medical record.");
     }
     return service.getMedicalRecordById(id);
   }
+
   @GetMapping("/medical-record-by-patient-id/{id}")
-  public List<MedicalRecord> findMedicalRecordByPatientId(@PathVariable int id){
+  public List<MedicalRecord> findMedicalRecordByPatientId(@PathVariable int id) {
     if (service.getMedicalRecordsByPatientId(id).isEmpty()) {
-      throw new CustomException("Cannot find this patient.");
+      return new ArrayList<MedicalRecord>();
     }
     return service.getMedicalRecordsByPatientId(id);
   }
